f3ccea6c609ff4ecb2e2e90cfda60ba9a6796e13
Amanda Rodrigues <amanda.nrodrigues@hotmail.com>
2021-04-03 16:56:35 -0400

    exemplo carregamento adiantado

--- Curso/Data/ApplicationContext.cs
+++ Curso/Data/ApplicationContext.cs
@@ -1,5 +1,6 @@
 ﻿using Curso.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,7 @@ namespace Curso.Data
 {
     public class ApplicationContext : DbContext
     {
+        
         public DbSet<Pedido> Pedidos { get; set; }
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Cliente> Clientes { get; set; }
--- Curso/Program.cs
+++ Curso/Program.cs
@@ -2,6 +2,8 @@
 using Curso.ValueObjects;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Curso
 {
@@ -17,10 +19,63 @@ namespace Curso
 
             //db.Database.Migrate();
 
-            InserirDados();
+            //InserirDados();
+            //ConsultarDados();
+            //CadastrarPedido();
+            ConsultarPedidoCarregamentoAdiantado();
         }
+        private static void ConsultarPedidoCarregamentoAdiantado()
+        {
+            using var db = new Data.ApplicationContext();
+            var pedidos = 
+                db.Pedidos
+                .Include(p=> p.Itens)
+                .ThenInclude(p=>p.Produto)
+                .ToList();
+            Console.WriteLine(pedidos.Count());
+        }
+        public static void CadastrarPedido()
+        {
+            using var db = new Data.ApplicationContext();
 
+            var cliente = db.Clientes.FirstOrDefault();
+            var produto = db.Produtos.FirstOrDefault();
 
+            var pedido = new Pedido
+            {
+                ClienteId = cliente.Id,
+                IniciadoEm = DateTime.Now,
+                Status = StatusPedido.Analise,
+                TipoFrete = TipoFrete.SemFrete,
+                Itens = new List<PedidoItem>
+                {
+                    new PedidoItem
+                    {
+                        ProdutoId = produto.Id,
+                        Desconto = 0,
+                        Quantidade = 1,
+                        Valor = 10
+                    }
+                }
+
+            };
+
+            db.Pedidos.Add(pedido);
+
+            db.SaveChanges();
+        }
+        public static void ConsultarDados()
+        {
+            using var db = new Data.ApplicationContext();
+            //var consultaPorSintaxe = (from c in db.Clientes where c.Id>0 select c).ToList();
+            var consultaPorMetodo = db.Clientes.Where(p => p.Id > 0).ToList();
+
+            foreach(var cliente in consultaPorMetodo)
+            {
+                Console.WriteLine($"Consultando Cliente: {cliente.Nome}");
+                db.Clientes.Find(cliente.Id);
+            }
+        }
         private static void InserirDados()
         {
             var produto = new Produto
@@ -31,11 +86,46 @@ namespace Curso
                 TipoProduto = TipoProduto.MercadoriaParaRevenda,
                 Ativo = true
             };
+            var listaClientes = new List<Cliente>
+            {
+                new Cliente
+                {
+                Nome = "Amanda Rodrigues2",
+                CEP = "79290000",
+                Cidade = "Bonito",
+                Estado = "MS",
+                Telefone = "991646461"
+                },
+                new Cliente
+                {
+                Nome = "Amanda Rodrigues3",
+                CEP = "79290000",
+                Cidade = "Bonito",
+                Estado = "MS",
+                Telefone = "991646461"
+                },
+                new Cliente{
+                Nome = "Amanda Rodrigues4",
+                CEP = "79290000",
+                Cidade = "Bonito",
+                Estado = "MS",
+                Telefone = "991646461"
+                }
+            };
+            var cliente = new Cliente
+            {
+                Nome = "Amanda Rodrigues",
+                CEP = "79290000",
+                Cidade = "Bonito",
+                Estado = "MS",
+                Telefone = "991646461"
+            };
 
             using var db = new Data.ApplicationContext();
             //db.Produtos.Add(produto);
             //db.Set<Produto>().Add(produto);
-            db.Add(produto);
+            //db.Add(cliente);
+            db.AddRange(listaClientes);
 
             var registros = db.SaveChanges();
             Console.WriteLine($"Total de registros:  : {registros}");
